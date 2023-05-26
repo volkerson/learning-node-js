0b178ee43c624fa9362d62fb7a4318ade3afdf7d
Ben <126400171+volkerson@users.noreply.github.com>
2023-05-27 00:14:55 +0200

    Create app.js

---
+++ express-js-path-router-static/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+// catch all - for error page
+app.use((req,res,next) => {
+    res
+    .status(404)
+    .sendFile(path.join(__dirname, './', 'views', '404.html'));
+});
+
+
+app.listen(3000);

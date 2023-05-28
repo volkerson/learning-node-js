2b58fc75632eafb388342cb81012aec81890a75e
Ben <126400171+volkerson@users.noreply.github.com>
2023-05-28 23:12:48 +0200

    Create app.js

---
+++ template-engines-handlebars/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const bodyParser = require('body-parser');
+const path = require('path');
+const expressHandlebars = require('express-handlebars');
+
+const app = express()
+
+// initialize handelbars engine
+app.engine('hbs', expressHandlebars({ 
+    layoutsDir: 'views/layout/', 
+    defaultLayout: 'default',
+    // add the file extension you use for handlebars
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+// set views folder 
+app.set('views', 'views')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// set directory for public folder that includes css-files, .js-files etc.
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+// catch all - for error page
+app.use((req, res, next) => {
+    res
+    .status(404)
+    //.sendFile(path.join(__dirname, './', 'views', '404.html'));
+    .render('404', { pageTitle: 'Page not found!'} )
+});
+
+
+app.listen(3000);
